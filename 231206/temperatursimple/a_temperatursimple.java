a7c9547a6e81d5dabb10f58ab45890b63b42fbfc
wani527 <nicolas.walisch@outlook.com>
2023-12-06 11:01:34 +0100

    renamed:    231206/a_temperatursimple.java -> 231206/temperatursimple/a_temperatursimple.java 	new file:   231206/temperatursimple/temperatur.java

--- 231206/temperatursimple/a_temperatursimple.java
+++ 231206/temperatursimple/a_temperatursimple.java
@@ -1,5 +1,5 @@
 public class a_temperatursimple {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        temperatur temp1 = new temperatur();
     }
 }
---
+++ 231206/temperatursimple/temperatur.java
@@ -0,0 +1,6 @@
+public class temperatur {
+    public temperatur() {
+            float temperatur = 16;
+            System.out.println(temperatur);
+    }
+}
