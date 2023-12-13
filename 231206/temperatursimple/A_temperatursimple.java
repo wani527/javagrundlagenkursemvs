205a40c596999dc7b9ddcd5beb080ae4cfdc0c19
wani527 <nicolas.walisch@outlook.com>
2023-12-13 09:09:42 +0100

    print temp1

--- 231206/temperatursimple/A_temperatursimple.java
+++ 231206/temperatursimple/A_temperatursimple.java
@@ -4,6 +4,7 @@ public class A_temperatursimple {
     public static void main(String[] args) {
         Scanner temperaturscanner = new Scanner(System.in);
         Temperatur temp1 = new Temperatur();
+        temp1.printCelsius();
         Temperatur temp2 = new Temperatur(18.322342156342525768345768453f);
         temp2.printCelsius();
         Temperatur temp3 = new Temperatur(temperaturscanner.nextFloat());
