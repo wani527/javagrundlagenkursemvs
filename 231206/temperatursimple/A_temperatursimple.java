b21c0f5dfbdccbd7c1ee4eb48deae023739b2584
wani527 <nicolas.walisch@outlook.com>
2023-12-13 08:34:30 +0100

    added temp3

--- 231206/temperatursimple/A_temperatursimple.java
+++ 231206/temperatursimple/A_temperatursimple.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class A_temperatursimple {
     public static void main(String[] args) {
+        Scanner temperaturscanner = new Scanner(System.in);
         Temperatur temp1 = new Temperatur();
         Temperatur temp2 = new Temperatur(18.322342156342525768345768453f);
         temp2.printCelsius();
+        Temperatur temp3 = new Temperatur(temperaturscanner.nextFloat());
+        temp3.printHotOrColdCelsius();
+        temperaturscanner.close();
     }
 }
