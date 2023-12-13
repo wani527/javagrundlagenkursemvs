3e97a295bc2e76622d4652905cedabf715ac884a
wani527 <nicolas.walisch@outlook.com>
2023-12-13 09:55:11 +0100

    updated main()

--- 231206/temperatursimple/A_temperatursimple.java
+++ 231206/temperatursimple/A_temperatursimple.java
@@ -2,17 +2,20 @@ import java.util.Scanner;
 
 public class A_temperatursimple {
     public static void main(String[] args) {
+
         Scanner temperaturscanner = new Scanner(System.in);
         Temperatur temp1 = new Temperatur();
         temp1.printCelsius();
         Temperatur temp2 = new Temperatur(18.322342156342525768345768453f);
         temp2.printCelsius();
+        System.err.print("Enter a Temperature: ");
         Temperatur temp3 = new Temperatur(temperaturscanner.nextFloat());
-        temp3.printHotOrColdCelsius();
         temp3.calculateFahrenheit();
         temp3.calculateKelvin();
         temp3.printFahrenheit();
         temp3.printKelvin();
+        System.err.print("Enter another Temperature: ");
+        temp3.printHotOrColdCelsius(temperaturscanner.nextFloat());
         temperaturscanner.close();
     }
 }
