a68f64ba99a5ab1306529726a3b82181fdf7d7dd
wani527 <nicolas.walisch@outlook.com>
2023-12-13 11:26:08 +0100

    temperaturaccessmodifier prepared

---
+++ 231206/temperaturaccessmodifier/Temperatur.java
@@ -0,0 +1,43 @@
+public class Temperatur {
+    float temperaturCelsius = 0f;
+    float temperaturFahrenheit = 0f;
+    float temperaturKelvin = 0f;
+    
+    public Temperatur() {
+            this.temperaturCelsius = 18.32f;
+    }
+    public Temperatur(float temperaturCelsius) {
+        this.temperaturCelsius = temperaturCelsius;
+    }
+
+    public void printCelsius() {
+        System.out.println(this.temperaturCelsius + "°C");
+    }
+    public void printFahrenheit() {
+        System.out.println(this.temperaturFahrenheit + "°F");
+    }
+    public void printKelvin() {
+        System.out.println(this.temperaturKelvin + "°K");
+    }
+
+    public void printHotOrColdCelsius(float temperatur) {
+        if (temperatur > 24) {
+            System.out.println("It's hot: " + temperatur + "° Celsius!");
+        } else {
+            System.out.println("Its freezing: " + temperatur + "° Celsius!");
+        }
+    }
+    public void printHotOrColdFahrenheit(float temperatur) {
+        System.err.println("ERROR: printHotOrColdFahrenheit not implemented! Ignoring");
+    }
+    public void printHotOrColdKelvin(float temperatur) {
+        System.err.println("ERROR: printHotOrColdKelvin not implemented! Ignoring");
+    }
+
+    public void calculateFahrenheit() {
+        temperaturFahrenheit = (this.temperaturCelsius * 1.8f) + 32f;
+    }
+    public void calculateKelvin() {
+        temperaturKelvin = this.temperaturCelsius + 273.15f;
+    }
+}
