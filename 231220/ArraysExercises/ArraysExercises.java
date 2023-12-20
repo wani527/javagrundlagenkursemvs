82b7e9a9bb196adc13538155aa276683b44b0d76
wani527 <nicolas.walisch@outlook.com>
2023-12-20 09:42:06 +0100

    modified:   231220/ArraysExercises/ArraysExercises.java

--- 231220/ArraysExercises/ArraysExercises.java
+++ 231220/ArraysExercises/ArraysExercises.java
@@ -2,7 +2,7 @@ public class ArraysExercises {
 	public static void main (String[] args) {
 		String[] names = new String[10];
 		names[0] = "Primis";
-		names[1] = "Hans";
+		names[1] = "wani527";
 		names[2] = "Peter";
 		names[3] = "Hanspeter";
 		names[4] = "Max";
@@ -10,7 +10,8 @@ public class ArraysExercises {
 		names[6] = "Peter-Hans";
 		names[7] = "Primis-Max";
 		names[8] = "Max-Primis";
-		names[9] = "Peter-Hans-Primis-Max-Hanspeter";
+		names[9] = "Hans";
 		System.out.println(names[5]);
+		System.out.println(names[1]);
 	}
 }
