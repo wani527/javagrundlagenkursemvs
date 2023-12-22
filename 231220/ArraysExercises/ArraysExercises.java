1de31247e18a7c4b2af150f657925c2d7d191438
wani527 <nicolas.walisch@outlook.com>
2023-12-22 14:51:29 +0100

    removed old code

--- 231220/ArraysExercises/ArraysExercises.java
+++ 231220/ArraysExercises/ArraysExercises.java
@@ -9,19 +9,5 @@ public class ArraysExercises {
 		myNames.printArrayValuesFromTo(4,6);
 		myNames.printArrayValuesFromTo(8,2);
 		myNames.printArrayValuesFromTo(5,5);
-/*		System.out.println(mynames[5]);
-		System.out.println(mynames[1]);
-		mynames[0] = "Primus";
-		System.out.println(names.length);
-		int i;
-		for (i=0; i<=9; i++) {
-			System.out.println(mynames[i]);
-		}
-		for (String name : mynames) {
-			System.out.println(name);
-		}
-		for (i=2; i<=(mynames.length - 2); i++) {
-			System.out.println(mynames[i]);
-		} */
 	}
 }
